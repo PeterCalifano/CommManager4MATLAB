4f0c13366d73e79f9d34ac694907a587693f27b4
PeterC <petercalifano.gs@gmail.com>
2024-11-27 11:15:42 +0100

    Added placeholder for future development

--- src/TensorCommManager.m
+++ src/TensorCommManager.m
@@ -1,5 +1,5 @@
 classdef TensorCommManager < CommManager
- %% CONSTRUCTOR
+    %% CONSTRUCTOR
     % self = TensorCommManager(i_charServerAddress, i_ui32ServerPort, i_dCommTimeout, kwargs)
     % -------------------------------------------------------------------------------------------------------------
     %% DESCRIPTION
@@ -22,11 +22,11 @@ classdef TensorCommManager < CommManager
     % [-]
     % -------------------------------------------------------------------------------------------------------------
     %% Function code
-
+    
     properties (SetAccess = protected, GetAccess = public)
-
+        
     end
-
+    
     methods
         function self = TensorCommManager(charServerAddress, ui32ServerPort, dCommTimeout, kwargs)
             arguments
@@ -34,66 +34,86 @@ classdef TensorCommManager < CommManager
                 ui32ServerPort    (1,1) uint32  {isscalar} = 55556
                 dCommTimeout      (1,1) double  {isscalar}  = 20
             end
-
+            
             arguments
                 kwargs.bUSE_PYTHON_PROTO  (1,1) logical {islogical} = true
                 kwargs.bUSE_CPP_PROTO     (1,1) logical {islogical} = false
                 kwargs.bInitInPlace       (1,1) logical {islogical} = false
+                kwargs.bMULTI_TENSOR      (1,1) logical {islogical} = false
             end
-
+            
             self = self@CommManager(charServerAddress, ui32ServerPort, dCommTimeout, ...
                 'bUSE_PYTHON_PROTO', kwargs.bUSE_PYTHON_PROTO, 'bUSE_CPP_PROTO', kwargs.bUSE_CPP_PROTO, 'bInitInPlace', kwargs.bInitInPlace);
-
+            
+            self.bMULTI_TENSOR = bMULTI_TENSOR;
         end
-
+        
         % PUBLIC METHODS
         function writtenBytes = WriteBuffer(self, dTensorArray)
             arguments
                 self          (1,1)
                 dTensorArray   double
             end
-
+            
             self.assertInit();
-
-
+            
+            
             % TODO
-
-            % Convert dTensorArray into ui8DataBuffer with "adaptive size convention"
-            ui8DataBuffer = TensorArray2Bytes(dTensorArray);
+            if self.bMULTI_TENSOR == true
+                error('NOT IMPLEMENTED YET')
+                % TODO: convert everything in a cell or struct into multiple single messages with "tensor convention"
+                % Then concat all messages into a single buffer
+                
+            else
+                ui8DataBuffer = TensorArray2Bytes(dTensorArray);
+                
+            end
+            
+            % Convert dTensorArray into ui8DataBuffer with "tensor convention"
             bAddDataSize = true;
-
+            
             % Call base class method to write raw buffer
             writtenBytes = self.WriteBuffer@CommManager(ui8DataBuffer, bAddDataSize);
-
-
+            
+            
         end
-
+        
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % Method to read data to buffer
         function [dTensorArray, self] = ReadBuffer(self)
-
+            
             self.assertInit();
-
+            
             % Read data incoming from TCP server
-           [ui32RecvBytes, ui8RecvDataBuffer, self] = self.ReadBuffer@CommManager();
-
-            % Convert received buffer into dTensorArray with "adaptive size convention"
-            dTensorArray = Bytes2TensorArray(ui32RecvBytes, ui8RecvDataBuffer);
-
+            [ui32RecvBytes, ui8RecvDataBuffer, self] = self.ReadBuffer@CommManager();
+            
+            
+            % TODO
+            if self.bMULTI_TENSOR == true
+                error('NOT IMPLEMENTED YET')
+                % TODO: convert everything in a cell or struct into multiple single messages with "tensor convention"
+                % Then concat all messages into a single buffer
+                
+            else
+                % Convert received buffer into dTensorArray with "tensor convention"
+                dTensorArray = Bytes2TensorArray(ui32RecvBytes, ui8RecvDataBuffer);
+            end
+            
+            
         end
-
+        
         % function [o_objdecodedMessage, self] = DecodeBuffer(self)
         %     arguments (Output)
         %         o_objdecodedMessage (1,1) {isValidDataStruct}
         %         self (1,1)
         %     end
-        % 
+        %
         %     error('NOT IMPLEMENTED YET')
         %     [self.recvDataStruct, self] = self.DecodeBuffer@CommManager(); % TODO
         % end
-
+        
     end
-
+    
     methods (Static, Access = public)
         function dTensorArray = Bytes2TensorArray(ui32RecvBytes, ui8DataBuffer)
             % Static method to convert buffer of bytes to tensor array (N-dim array) (format tailored for PyTorchAutoForge)
@@ -101,23 +121,23 @@ classdef TensorCommManager < CommManager
                 ui32RecvBytes (1, 1) uint32 {isscalar}
                 ui8DataBuffer (1, :) uint8  {isvector}
             end
-
+            
             % Read bytes indicating how many sizes are to unpack
             i32TensorDims = typecast(ui8DataBuffer(1:4), 'int32');
             
             % Create shape array
             i32TensorShape = zeros(1, i32TensorDims, "uint32");
-
+            
             for idPtr = 1:i32TensorDims
                 i32TensorShape(idPtr) = typecast(ui8DataBuffer(4*idPtr+1:4*idPtr+4), 'uint32');
             end
             
             % Assign idPtr to next byte
             idPtr = 4*i32TensorDims + 4; % TBC
-
+            
             % Check ptr is lower than total bytes
             assert(idPtr < ui32RecvBytes, 'ERROR: Pointer exceeds total bytes.')
-
+            
             % Get bytes of tensor data from buffer and do typecasting
             dTensorBuffer = typecast(ui8DataBuffer(idPtr+1:end), 'single');
             
@@ -128,9 +148,9 @@ classdef TensorCommManager < CommManager
             if any(i32TensorShape == 1)
                 dTensorArray = squeeze(dTensorArray);
             end
-
+            
         end
-
+        
         function [ui8DataBuffer, ui32TensorDims, ui32TensorShape] = TensorArray2Bytes(dTensorArray, kwargs)
             % Static method to convert tensor array (N-dim array) to buffer of bytes to transmit over TCP (format tailored for PyTorchAutoForge)
             arguments
@@ -140,19 +160,19 @@ classdef TensorCommManager < CommManager
                 kwargs.ui32NumOfBatches  (1,1) uint32 {isscalar} = 0
                 kwargs.ui32NumOfChannels (1,1) uint32 {isscalar} = 0
             end
-
+            
             % Get tensor shape
             ui32TensorShape = uint32(size(dTensorArray));
             
             % Get number of dimensions
             ui32TensorDims = uint32(length(ui32TensorShape));
-
+            
             % If specified, add number of batches and channels to shape
             if kwargs.ui32NumOfBatches > 0
                 ui32TensorShape = [kwargs.ui32NumOfBatches, ui32TensorShape];
                 ui32TensorDims = ui32TensorDims + 1;
             end
-
+            
             if kwargs.ui32NumOfChannels > 0
                 ui32TensorShape = [ui32TensorShape, kwargs.ui32NumOfChannels];
                 ui32TensorDims = ui32TensorDims + 1;
@@ -160,24 +180,24 @@ classdef TensorCommManager < CommManager
             
             % Flatten tensor data to linear array and downcast to single
             fTensorBuffer = single(dTensorArray(:));
-
+            
             % Convert tensor data to bytes
             ui8TensorBuffer = typecast(fTensorBuffer, 'uint8');
-
+            
             % Convert tensor shape to bytes
             ui8TensorDims = typecast(ui32TensorDims, 'uint8');
             ui8TensorShape = typecast(ui32TensorShape, 'uint8');
             
             % Construct data buffer
             ui8DataBuffer = [ui8TensorDims, ui8TensorShape, ui8TensorBuffer];
-
+            
         end
     end
-
+    
     %% PRIVATE METHODS
     methods (Access=private)
-
-
+        
+        
     end
-
+    
 end
