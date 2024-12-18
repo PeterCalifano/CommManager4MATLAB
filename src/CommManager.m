0270ebddb71a5ce2420aa367a754ea5e249e8a3f
PeterC <petercalifano.gs@gmail.com>
2024-12-18 23:02:34 +0100

    Added basic functions to serialize and deserialize data using msg-pack

--- src/CommManager.m
+++ src/CommManager.m
@@ -1,51 +1,52 @@
 classdef CommManager < handle
-%% CONSTRUCTOR 
-% self = CommManager(i_charServerAddress, i_ui32ServerPort, i_dCommTimeout, kwargs)
-% -------------------------------------------------------------------------------------------------------------
-%% DESCRIPTION
-% MATLAB class to wrap tcpclient (which cannot be inherited), providing conveniency methods to interact with
-% external TCP servers. See section "methods" for details.
-% -------------------------------------------------------------------------------------------------------------
-%% DATA MEMBERS
-% charServerAddress
-% ui32ServerPort
-% dCommTimeout
-% objtcpClient
-% recvDataBuffer = 0;
-% ui8CommMode = 0;
-% bufferToWrite = 0;
-% recvDataStruct = struct();
-% dataStructToWrite = struct();
-% bUSE_PYTHON_PROTO = true;
-% bUSE_CPP_PROTO = false;
-% bCommManagerReady = false;
-% -------------------------------------------------------------------------------------------------------------
-%% METHODS
-% CommManager
-% Disconnect    
-% Initialize    
-% WriteBuffer   
-% DecodeBuffer  
-% EncodeBuffer  
-% ReadBuffer    
-% -------------------------------------------------------------------------------------------------------------
-%% CHANGELOG
-% 12-06-2024        Pietro Califano      Initial implementation as prototype
-% 20-06-2024        Pietro Califano      Debug and updated; Added disconnection method.
-% 14-11-2024        Pietro Califano      Updated class for improved generality; removed code specific to
-%                                        robots-API usage (moved to RobotsCommManager subclass)
-% 24-11-2024        Pietro Califano      Moved to dedicated repo on GitHub for new dev. course: CommManager4MATLAB
-% -------------------------------------------------------------------------------------------------------------
-%% DEPENDENCIES
-% [-]
-% -------------------------------------------------------------------------------------------------------------
-%% Future upgrades
-% 1) Implement protobuf handling
-% -------------------------------------------------------------------------------------------------------------
-%% Function code
+    %% CONSTRUCTOR
+    % self = CommManager(i_charServerAddress, i_ui32ServerPort, i_dCommTimeout, kwargs)
+    % -------------------------------------------------------------------------------------------------------------
+    %% DESCRIPTION
+    % MATLAB class to wrap tcpclient (which cannot be inherited), providing conveniency methods to interact with
+    % external TCP servers. See section "methods" for details.
+    % -------------------------------------------------------------------------------------------------------------
+    %% DATA MEMBERS
+    % charServerAddress
+    % ui32ServerPort
+    % dCommTimeout
+    % objtcpClient
+    % recvDataBuffer = 0;
+    % ui8CommMode = 0;
+    % bufferToWrite = 0;
+    % recvDataStruct = struct();
+    % dataStructToWrite = struct();
+    % bUSE_PYTHON_PROTO = true;
+    % bUSE_CPP_PROTO = false;
+    % bCommManagerReady = false;
+    % -------------------------------------------------------------------------------------------------------------
+    %% METHODS
+    % CommManager
+    % Disconnect
+    % Initialize
+    % WriteBuffer
+    % DecodeBuffer
+    % EncodeBuffer
+    % ReadBuffer
+    % -------------------------------------------------------------------------------------------------------------
+    %% CHANGELOG
+    % 12-06-2024        Pietro Califano      Initial implementation as prototype
+    % 20-06-2024        Pietro Califano      Debug and updated; Added disconnection method.
+    % 14-11-2024        Pietro Califano      Updated class for improved generality; removed code specific to
+    %                                        robots-API usage (moved to RobotsCommManager subclass)
+    % 24-11-2024        Pietro Califano      Moved to dedicated repo on GitHub for new dev. course: CommManager4MATLAB
+    % 18-12-2024        Pietro Califano      Added methods to use msg-pack library for serialization/de-serialization
+    % -------------------------------------------------------------------------------------------------------------
+    %% DEPENDENCIES
+    % [-]
+    % -------------------------------------------------------------------------------------------------------------
+    %% Future upgrades
+    % 1) Implement protobuf handling
+    % -------------------------------------------------------------------------------------------------------------
+    %% Function code
     
     %% PUBLIC DATA MEMBERS
-
+    
     properties (SetAccess = protected, GetAccess = public)
         charServerAddress
         ui32ServerPort
@@ -60,55 +61,55 @@ classdef CommManager < handle
         bUSE_CPP_PROTO = false;
         bCommManagerReady = false;
     end
-
+    
     %% PUBLIC METHODS
     methods (Access = public)
         % CONSTRUCTOR
         function self = CommManager(charServerAddress, ui32ServerPort, dCommTimeout, kwargs)
             arguments
-                charServerAddress (1,:) {ischar, isstring} 
+                charServerAddress (1,:) {ischar, isstring}
                 ui32ServerPort    (1,1) uint32   {isscalar}
-                dCommTimeout      (1,1) double  {isscalar} = 10     
+                dCommTimeout      (1,1) double  {isscalar} = 10
             end
-
+            
             arguments
                 kwargs.bUSE_PYTHON_PROTO  (1,1) logical {islogical} = true
                 kwargs.bUSE_CPP_PROTO     (1,1) logical {islogical} = false
                 kwargs.bInitInPlace       (1,1) logical {islogical} = false
             end
-
+            
             disp('Creating communication manager object...')
-
+            
             % Assign server address
             self.charServerAddress = charServerAddress;
             self.ui32ServerPort    = ui32ServerPort;
             self.dCommTimeout      = dCommTimeout;
-
+            
             % Assign server properties
             self.bUSE_PYTHON_PROTO = kwargs.bUSE_PYTHON_PROTO;
             self.bUSE_CPP_PROTO    = kwargs.bUSE_CPP_PROTO;
-
+            
             if kwargs.bInitInPlace
                 self = self.Initialize();
             end
             
         end
-
+        
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % Method to initialize tcpclient object to establish connection
         function self = Initialize(self)
             
             fprintf('\nInitializing communication calling tcpclient with HOST: %s, PORT: %s\n', ...
                 self.charServerAddress, num2str(self.ui32ServerPort));
-
+            
             self.objtcpClient = tcpclient(self.charServerAddress, self.ui32ServerPort, ...
                 "Timeout", self.dCommTimeout);
             
             self.bCommManagerReady = true;
             fprintf('Connection established corrected.');
-
+            
         end
-
+        
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % Method to write buffer (no check on format) and send data to server
         function writtenBytes = WriteBuffer(self, dataBuffer, bAddDataSize)
@@ -117,74 +118,46 @@ classdef CommManager < handle
                 dataBuffer   (1,:)
                 bAddDataSize (1,1) = false
             end
-
+            
             self.assertInit();
-
+            
             if not(isa(dataBuffer, 'uint8'))
                 error('Input dataBuffer is not a stream of uint8 (bytes)!')
             end
-
+            
             fprintf("\nCommManager: Writing buffer of %d bytes...\n", whos('dataBuffer').bytes);
-
+            
             if bAddDataSize
                 dataLength = typecast(uint32(length(dataBuffer)), 'uint8');
                 dataBuffer = [dataLength, dataBuffer];
             end
-
+            
             write(self.objtcpClient, dataBuffer, "uint8");
             writtenBytes = self.objtcpClient.NumBytesAvailable;
-
+            
             % Flush data from buffer
             flush(self.objtcpClient, "output");
-
+            
         end
-
+        
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % Method to read data to buffer
         function [recvBytes, recvDataBuffer, self] = ReadBuffer(self)
-
+            
             self.assertInit();
-
+            
             % Read first 4 bytes to get message buffer length
             recvBytes   = read(self.objtcpClient, 4, 'uint8');
             recvBytes   = typecast(recvBytes, 'uint32');
-
+            
             % Read message buffer
             recvDataBuffer = read(self.objtcpClient, recvBytes, 'uint8');
-
+            
             % Allocate buffer to property
             self.recvDataBuffer = recvDataBuffer;
-
+            
             % Flush data from buffer
             flush(self.objtcpClient, "input");
-        end
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Method to encode input datastruct through protobuflib % TODO
-        % NOTE: THIS MUST BE GENERIC FOR ANY PROTO --> which TBS by caller
-        function [encodedMessage] = EncodeBuffer(i_objdataStructToWrite)
-            arguments
-                i_objdataStructToWrite (1,1) {isValidDataStruct}
-            end
-
-            self.assertInit();
-
-            error('NOT IMPLEMENTED YET')
-            commManager.bufferToWrite;  % TODO
-
-        end
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Method to decode datastruct through protobuflib  % TODO
-        function [o_objdecodedMessage, self] = DecodeBuffer(self)
-            arguments (Output)
-                o_objdecodedMessage (1,1) {}
-                self (1,1)
-            end
-            self.assertInit();
-
-            error('NOT IMPLEMENTED YET')
-            self.recvDataStruct = o_objdecodedMessage; % TODO
         end
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -192,24 +165,50 @@ classdef CommManager < handle
         function [self] = Disconnect(self)
             self.objtcpClient = 0;
         end
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Method to write message from datastruct
-
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Method to read message from datastruct
-
-
+        
     end
-
-
+    
+    methods (Static)
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Method to encode input datastruct through protobuflib % TODO
+        % NOTE: THIS MUST BE GENERIC FOR ANY PROTO --> which TBS by caller
+        function [ui8SerializedBuffer] = SerializeBuffer(inDataStruct, bSplitMessages)
+            arguments
+                inDataStruct (1,1) {isValidDataStruct}
+                bSplitMessages (1,1) logical {islogical} = false
+            end
+            
+            % TODO: add "multi message mode" --> input is a cell containing multiple datastructs to be serialized as different messages
+            self.assertInit();
+            if bSplitMessages == false
+                ui8SerializedBuffer = dumpmsgpack(inDataStruct);        
+            end    
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Method to decode datastruct through protobuflib  % TODO
+        function [outDataStruct, self] = DeserializeBuffer(self, ui8SerializedBuffer)
+            arguments
+                self
+                ui8SerializedBuffer (1,1)
+            end
+            
+            self.assertInit();
+            outDataStruct = parsemsgpack(ui8SerializedBuffer);
+        end
+    end
+    
     methods (Access = protected)
         % DEVNOTE: may be implemented by overloading/overriding the subsref method such that it gets called
         % at each dot indexing (properties included) automatically.
         function assertInit(self)
             assert(self.bCommManagerReady == true, 'Class not initialized correctly. You need to establish connection before use! Call instance.Initialize() or pass flag bInitInPlace as true.')
         end
+        
+        function bIsValid = isValidDataStruct(objdataStructToWrite)
+            % TEMPORARY: to be implemented
+            bIsValid = true;
+        end
     end
-
+    
 end
