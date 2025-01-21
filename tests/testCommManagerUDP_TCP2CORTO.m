3982458151b17850c19d446c34dd2c002597acbb
PeterC <petercalifano.gs@gmail.com>
2025-01-20 22:56:28 -1000

    Minor change

--- tests/testCommManagerUDP_TCP2CORTO.m
+++ tests/testCommManagerUDP_TCP2CORTO.m
@@ -2,13 +2,13 @@ close all
 clear
 clc
 
-% Unit test for CORTO interfaces and CommManager (S5 Didymos Milani scenario)
+% Unit test for CORTO interfaces and CommManager (using S5 Didymos Milani scenario)
 % SUN:   POS [-1.87979488e+08  2.98313209e+04  5.99584851e+07] - Q [ 0.57104144  0.41702718 -0.41731124 -0.57083389]
 % SC:    POS [0. 0. 0.] - Q [1. 0. 0. 0.]
 % BODY (0):   POS: [  0.16706893   0.0235506  -13.93270439] - Q [-0.05240883 -0.20821087  0.76502471  0.60715537]
 % BODY (1):   POS: [  0.96841929   0.38199128 -13.145638  ] - Q [-0.17513088 -0.35985445  0.70648485  0.58370726]
 
-%% Load_test_data
+% Load_test_data
 % From milani-gnc simulation
 dSunPos  = [-1.87979488e+08  2.98313209e+04  5.99584851e+07];
 dSunQuat = [ 0.57104144  0.41702718 -0.41731124 -0.57083389];
@@ -22,12 +22,12 @@ dBody2Pos = [  0.96841929   0.38199128 -13.145638  ]; % [BU]
 dBody1Quat = [-0.05240883 -0.20821087  0.76502471  0.60715537]; % [BU] 
 dBody2Quat = [-0.17513088 -0.35985445  0.70648485  0.58370726]; % [BU]
 
-% BlenderModelPath = '/home/peterc/devDir/rendering-sw/corto_PeterCdev/input/OLD_ones_0.1/S5_Didymos_Milani/S5_Didymos_Milani.blend';
+BlenderModelPath = '/home/peterc/devDir/rendering-sw/corto_PeterCdev/input/OLD_ones_0.1/S5_Didymos_Milani/S5_Didymos_Milani.blend';
 
 charScriptName = 'CORTO_UDP_TCP_interface.py';
 % charScriptName = 'CORTO_to_Simulink_HF_1_d.py';
 
-BlenderModelPath = "/home/peterc/devDir/projects-DART/data/rcs-1/pre-phase-A/blender/Apophis_RGB.blend";
+% BlenderModelPath = "/home/peterc/devDir/projects-DART/data/rcs-1/pre-phase-A/blender/Apophis_RGB.blend";
 
 % CORTO_pyInterface_path  = 'script/CORTO_interfaces/corto_PeterCdev/server_api/CORTO_UDP_TCP_interface.py';
 % CORTO_pyInterface_path = '/home/peterc/devDir/projects-DART/milani-gnc/script/CORTO_interfaces/corto_PeterCdev/scripts/';
@@ -72,10 +72,8 @@ writtenBytes = objCommManager.WriteBuffer(ui8Buffer, false, ui32TargetPort);
 % Test read
 [recvBytes, recvDataBuffer, self] = objCommManager.ReadBuffer(); 
 
-% 
-recvDataVector = typecast(recvDataBuffer, 'double');
-
 % Cast buffer to double and display image
+recvDataVector = typecast(recvDataBuffer, 'double');
 dImgRGB = UnpackImageFromCORTO(recvDataVector);
 imshow(dImgRGB);
 
@@ -85,17 +83,9 @@ imshow(dImgRGB);
 % Close connection by deleting CommManager object
 clear objCommManager
 
-%% CORTOpyCommManager_high_level_class
-
-% Define cortopy comm. manager object initializing in place
-objCortopyCommManager = CORTOpyCommManager(charServerAddress, ui32ServerPort, dCommTimeout, ...
-    'bInitInPlace', true);
-
-
-
 return
 
-%% Kill_blender_process
+% Kill_blender_process
 % Code to kill Blender processes
 % Find the process
 [~, result] = system('pgrep -f blender'); % Get the process ID(s) of blender
